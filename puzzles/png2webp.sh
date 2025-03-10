4cdb71f827f47eedab73970b9409b2a86f380e80
Sapium59 <sapium59@gmail.com>
2025-03-10 13:27:02 +0800

    amend: r2q2 webp reso

--- puzzles/png2webp.sh
+++ puzzles/png2webp.sh
@@ -1,7 +1,7 @@
-# default: png -> webp
-for file in *.png; do
-    ffmpeg -i "$file" "${file%.png}.webp"
-done
+# # default: png -> webp
+# for file in *.png; do
+#     ffmpeg -i "$file" "${file%.png}.webp"
+# done
 
 
 # # r2q5 (ARCAEA): quality = 10%, wh = 33% (=1280x800)
@@ -10,7 +10,8 @@ done
 # done
 
 
-# # r2q2 (AREA): quality = 1%, wh = 25%
-# for file in r2q2*.png; do
-#     ffmpeg -i "$file" -vf "scale=iw/4:ih/4" -qscale:v 1 "${file%.png}.webp"
-# done
+# r2q2 (AREA): quality = 50%, wh = 25%
+for file in r2q2*.png; do
+    # mv $file "${file%.png}.25.1.webp"
+    ffmpeg -i "$file" -vf "scale=iw/4:ih/4" -qscale:v 50 "${file%.png}.webp"
+done
